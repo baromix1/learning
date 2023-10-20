d89720b70143fc45930a2ad234520ef8d9ccc3a3
baromix1 <92299971+baromix1@users.noreply.github.com>
2023-10-20 19:28:55 +0200

    still learning
    added infrastructure and core, use of database and checking with postman

--- udemi/skinet/API/Controllers/ProductsController.cs
+++ udemi/skinet/API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
 
 namespace API.Controllers
 {
@@ -6,14 +9,24 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+
+
         [HttpGet]
-        public string GetProducts(){
-            return "string of products todo";
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            var products=await _context.Products.ToListAsync();
+
+            return products;
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id){
-            return "string of product todo";
+        public async Task<ActionResult<Product>> GetProduct(int id){
+            return await _context.Products.FindAsync(id);
         }
     }
 }
