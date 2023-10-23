af6313483ef9c8c0ae03c5d214ade6284001240e
baromix1 <92299971+baromix1@users.noreply.github.com>
2023-10-23 22:50:41 +0200

    still learning
    added generic repository

--- udemi/skinet/API/Controllers/ProductsController.cs
+++ udemi/skinet/API/Controllers/ProductsController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
 using Core.Entities;
 using Core.Interfaces;
 
@@ -10,30 +8,36 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository _repo;
-        public ProductsController(IProductRepository repo)
+        public readonly IGenericRepository<Product> _productRepo;
+        public readonly IGenericRepository<ProductBrand> _productBrandRepo;
+        public readonly IGenericRepository<ProductType> _productTypeRepo;
+
+        public ProductsController(IGenericRepository<Product>productRepo,
+        IGenericRepository<ProductBrand>productBrandRepo, IGenericRepository<ProductType>productTypeRepo)
         {
-            _repo=repo;
+            _productRepo = productRepo;
+            _productBrandRepo = productBrandRepo;
+            _productTypeRepo = productTypeRepo;
         }
 
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts(){
-            var products=await _repo.GetProductsAsync();
+            var products=await _productRepo.ListAllAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id){
-            return await _repo.GetProductByIdAsync(id);
+            return await _productRepo.GetByIdAsync(id);
         }
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands(){
-            return Ok(await _repo.GetProductBrandsAsync());
+            return Ok(await _productBrandRepo.ListAllAsync());
         }
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes(){
-            return Ok(await _repo.GetProductTypesAsync());
+            return Ok(await _productTypeRepo.ListAllAsync());
         }
     }
 }
---
+++ udemi/skinet/Core/Specification/BaseScpecification.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Core.Specification
+{
+    public class BaseScpecification<T> : ISpecification<T>
+    {
+        public BaseScpecification(Expression<Func<T,bool>> criteria)
+        {
+            Criteria=criteria;
+        }
+
+        public Expression<Func<T, bool>> Criteria {get; }
+
+        public List<Expression<Func<T, object>>> Includes {get; }=
+            new List<Expression<Func<T,object>>>();
+        
+        protected void AddInclude(Expression<Func<T,object>> includeExpression){
+            Includes.Add(includeExpression);
+        }
+        
+    }
+}
