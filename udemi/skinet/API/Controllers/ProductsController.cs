51d63fac2e14cac2a421daf8739c411c95a029dd
baromix1 <92299971+baromix1@users.noreply.github.com>
2023-10-25 16:34:00 +0200

    still learning
    added static output from api and fixed picture Urls

--- udemi/skinet/API/Controllers/ProductsController.cs
+++ udemi/skinet/API/Controllers/ProductsController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specification;
+using API.Dtos;
+using AutoMapper;
 
 namespace API.Controllers
 {
@@ -12,10 +14,12 @@ namespace API.Controllers
         public readonly IGenericRepository<Product> _productRepo;
         public readonly IGenericRepository<ProductBrand> _productBrandRepo;
         public readonly IGenericRepository<ProductType> _productTypeRepo;
+        private readonly IMapper _mapper;
 
         public ProductsController(IGenericRepository<Product>productRepo,
-        IGenericRepository<ProductBrand>productBrandRepo, IGenericRepository<ProductType>productTypeRepo)
+        IGenericRepository<ProductBrand>productBrandRepo, IGenericRepository<ProductType>productTypeRepo,IMapper mapper)
         {
+            _mapper = mapper;
             _productRepo = productRepo;
             _productBrandRepo = productBrandRepo;
             _productTypeRepo = productTypeRepo;
@@ -23,16 +27,17 @@ namespace API.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(){
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(){
             var spec=new ProductsWithTypersAndBrandSpecification();
             var products=await _productRepo.ListAsync(spec);
-            return Ok(products);
+            return Ok(_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id){
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id){
             var spec=new ProductsWithTypersAndBrandSpecification(id);
-            return await _productRepo.GetEntityWithSpec(spec);
+            var product = await _productRepo.GetEntityWithSpec(spec);
+            return _mapper.Map<Product,ProductToReturnDto>(product);
         }
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands(){
