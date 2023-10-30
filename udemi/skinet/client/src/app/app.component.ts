702533c851cbcc943d70430a2e793880a623b748
baromix1 <92299971+baromix1@users.noreply.github.com>
2023-10-29 22:46:44 -0300

    still learning
    started angular project of client
    added nav-bar and collecting simple data from api

---
+++ udemi/skinet/client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Skinet';
+  products: any[]=[];
+  
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=18').subscribe({
+      next:(response: any)=>this.products=response.data,
+      error: error=>console.log(error),
+      complete: ()=>{
+        console.log('request completed');
+        console.log('extra statement');
+      }
+    })
+  }
+}
