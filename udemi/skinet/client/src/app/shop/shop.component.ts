4a4cc6d37e9db485be477a6cde6cd5e977f8c6f9
baromix1 <92299971+baromix1@users.noreply.github.com>
2023-11-01 17:17:57 +0100

    learning still
    added filtering sorting

--- udemi/skinet/client/src/app/shop/shop.component.ts
+++ udemi/skinet/client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/product';
 import { ShopService } from './shop.service';
+import { Type } from '../shared/models/type';
+import { Brand } from '../shared/models/brand';
 
 @Component({
   selector: 'app-shop',
@@ -9,16 +11,56 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent  implements OnInit  {
   products:Product[]=[];
+  brands:Brand[]=[];
+  types:Type[]=[];
+  brandIdSelected=0;
+  typeIdSelected=0;
+  sortSelected='name';
+  sortOptions=[
+    {name:'Alphabetical',value:'name'},
+    {name:'Price: lowest to highest',value:'priceAsc'},
+    {name:'Price: highest to lowest',value:'priceDesc'},
+  ]
 
   constructor(private shopService: ShopService){}
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
-      next: response=>this.products=response.data,
-      error: error=>console.log(error)
-
-    })
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
     
   }
+getProducts(){
+  this.shopService.getProducts(this.brandIdSelected,this.typeIdSelected,this.sortSelected).subscribe({
+    next: response=>this.products=response.data,
+    error: error=>console.log(error)
 
+  })
+}
+getBrands(){
+  this.shopService.getBrands().subscribe({
+    next: response=>this.brands=[{id:0,name:'All'},...response],
+    error: error=>console.log(error)
+
+  })
+}
+getTypes(){
+  this.shopService.getTypes().subscribe({
+    next: response=>this.types=[{id:0,name:'All'},...response],
+    error: error=>console.log(error)
+
+  })
+}
+onBrandSelected(brandId:number){
+  this.brandIdSelected=brandId;
+  this.getProducts();
+}
+onTypeSelected(typeId:number){
+  this.typeIdSelected=typeId;
+  this.getProducts();
+}
+onSortSelected(event:any){
+this.sortSelected=event.target.value;
+this.getProducts();
+}
 }
